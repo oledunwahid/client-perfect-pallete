5c71822c392d563fb74adab3a75cbf379f9b6177
Khaled Makkawirelang <makkawirelang@gmail.com>
2024-07-30 21:02:42 +0700

    first setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#2F4156",
+        teal: "#567C8D",
+        skyblue: "#C8D9E6",
+        beige: "#F5EFEB",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};

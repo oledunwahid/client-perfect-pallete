c75cb0fedd267c52cd94bf5c458d107cfd0ca7ab
Rhazes Devino <rhazesd@gmail.com>
2024-07-31 22:27:37 +0800

    fix basic structure for client side

---
+++ src/pages/home/sections/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div>
+      <main className="">
+        <h1>Welcome to Perfect Palette</h1>
+        <p>Content goes here</p>
+      </main>
+    </div>
+  );
+}
+
+export default Hero;

150d8b1bd9eb1d24d8d16b8f1b2a296094f15642
Khaled Makkawirelang <makkawirelang@gmail.com>
2024-08-01 00:19:09 +0700

    adding hero card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import Categories from "./sections/Categories";
 import Hero from "./sections/Hero";
+import HeroCard from "./components/HeroCard";
 
 function Home() {
   return (
     <>
       <Hero />
+      <div className="bg-beige container mx-auto px-4 py-12">
+        <HeroCard />
+      </div>
       <Categories />
     </>
   );
